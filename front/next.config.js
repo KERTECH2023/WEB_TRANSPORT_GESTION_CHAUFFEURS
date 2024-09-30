69ab8958867d806ae9766e8da0f90c160686f17b
khalil-kertechnologie <khalil.ghoul@kertechnologie.fr>
2024-09-30 10:36:20 +0100

    Update next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['storage.googleapis.com'],
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
+  // cors
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
       },
-}
+    ];
+  },
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
