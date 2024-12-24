58fa43dcf8aa508d041705aba024ce7237522bae
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-12-24 09:23:27 +0100

    Update ChauffContro.js

--- back/Controllers/ChauffContro.js
+++ back/Controllers/ChauffContro.js
@@ -348,18 +348,7 @@ async function sendConfirmationEmail(Email, Nom) {
         </div>
 
         <div class="footer">
-            <div class="download-section">
-             <h2>Téléchargez notre application</h2>
-<div class="store-buttons">
-    <a href="https://apps.apple.com/app/flash-driver/id6737412071" class="store-button" target="_blank" rel="noopener noreferrer">
-        App Store
-    </a>
-    <a href="https://play.google.com/store/apps/details?id=com.tunisieuber.clientapp" class="store-button" target="_blank" rel="noopener noreferrer">
-        Google Play
-    </a>
-</div>
-
-                </div>
+      
             </div>
         </div>
     </div>
