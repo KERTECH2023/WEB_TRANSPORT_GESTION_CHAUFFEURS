d7a9a9f503736af09f9c2cbc46303a067f619e9c
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-02-28 15:42:44 +0100

    Update VoitureRoutes.js

--- back/routes/VoitureRoutes.js
+++ back/routes/VoitureRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const  router = express.Router()
 
 
-const VoitureCon  = require('../Controllers/VoitureContro')
+const VoitureCon  = require('../Controllers/ChauffControfrance');
 
 const UploadImage = require ("../services/upload");
 
