42a42b0a030dd09f5023a9fb9a8ecc2e48f9193e
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-02-28 15:54:04 +0100

    Update chauffeurfrranceRoute.js

--- back/routes/chauffeurfrranceRoute.js
+++ back/routes/chauffeurfrranceRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const ChauffContro = require("../Controllers/ChauffContro");
+const ChauffContro = require("../Controllers/ChauffControfrance");
 
 const UploadImage = require("../services/upload");
 
