cee2d6ac29900670e7bc2fddaeba5cdacf9e4847
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-03 15:50:44 +0100

    Update VoiturefranceController.js

--- back/Controllers/VoiturefranceController.js
+++ back/Controllers/VoiturefranceController.js
@@ -1,50 +1,87 @@
-const mongoose  =require('mongoose')
-const Schema  =mongoose.Schema
-const validator = require('validator');
-const { db2france } = require("../configbasefrance"); // Importer db2
-
-const VoitSchema = new mongoose.Schema(
-    {
-
-    immatriculation: {
-            type: String,
-          
-            
-        },
-        modelle: {
-            type: String,
-          
-            
-        },
-
-        cartegrise: {
-            type: String,
-            
-        },
-        assurance: {
-            type: String,
-           
-        },
-
-        chauffeur: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Chauffeur"
-        },
+const Voiture = require ("../Models/Voiturefrance")
 
 
+
+exports.addvoiture = async (req, res) => {
+    const { modelle , immatriculation} = req.body;
+    const chauffeurId = req.params.id;
+    
+ 
+// const {firebaseUrl} =req.file ? req.file : "";
+
+
+
+
+const cartegriseUrl = req.uploadedFiles.photoCartegrise || '';
+const assuranceUrl = req.uploadedFiles.photoAssurance || '';
+console.log("cate:",cartegriseUrl)
+
+
+
+
+
+    const verifUtilisateur = await Voiture.findOne({ immatriculation });
+    if (verifUtilisateur) {
+      res.status(403).send({ message: "Voiture existe deja !" });
+    } else {
+      const nouveauUtilisateur = new Voiture();
+
+  
+
+
+
+      nouveauUtilisateur.modelle = modelle;
+      nouveauUtilisateur.immatriculation = immatriculation;
+      nouveauUtilisateur.cartegrise = cartegriseUrl;
+      nouveauUtilisateur.assurance = assuranceUrl;
+      nouveauUtilisateur.chauffeur = chauffeurId;
+
+   
+  
+      console.log (
+        nouveauUtilisateur
+    )
+
+
+    
+      nouveauUtilisateur.save();
+  
+     
+      // token creation
+      res.status(201).send({ message: "success" });
     }
-    );
+  };
 
+//   exports.getBychauff = async (req, res) => {
+//     res.send({
+//         rec: await Voiture.find({chauffeur: req.params.id})
+            
+//     })
+// }
 
-
-    VoitSchema.set('toJSON', {
-    virtuals: true,
-    versionKey: false,
-    transform: function (doc, ret) {
-        delete ret._id;
+// exports.getBychauff = async(req,res,data) =>{
+   
+//     Voiture.find({ chauffeur: req.params.id },(err, data)=>{
+      
+//         res.send(data);
+//         console.log(data)
         
-    }
-});
+//     });
+// }
 
-const Voiture = db2france.model('Voiture',VoitSchema)
-module.exports = Voiture
+exports.getBychauff = async (req, res) => {
+    Voiture.find({ chauffeur: req.params.id })
+      .then(data => {
+        if (!data) {
+          res.status(404).send({ message: "Chauffeur introuvable pour id " + chauffeur });
+        } else {
+          // Extract the first element from the array
+          const response = data[0];
+          res.send(response);
+        }
+      })
+      .catch(err => {
+        res.status(500).send({ message: "Erreur récupération Chauffeur avec id=" + chauffeur });
+      });
+  }
+  
